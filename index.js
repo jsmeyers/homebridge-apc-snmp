020de74e2b8954bdd4846fbf1df6377e6791c72c
jsmeyers <jsmeyers@users.noreply.github.com>
2017-01-06 18:04:37 -0500

    beautify

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var apcUps = require("./snmp.js");
 var ipaddress = require("./ipaddress");
 var ups = new apcUps(ipaddress);
 
-module.exports = function (homebridge) {
+module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
     homebridge.registerAccessory("homebridge-apc-snmp", "APC", APCAccessory);
@@ -17,51 +17,53 @@ function APCAccessory(log, config) {
     this.lastupdate = 0;
 }
 
-APCAccessory.prototype =
-    {
-        getState: function (callback) {
-		ups.getTemperature(function(err, temperature) {
-  		if (err) {
-    		console.log(err.toString());
-callback(error);    		
-return;
-  		}	
-		this.log('The current temperature is:', temperature, 'C');
-		this.temperature = temperature;
-		temperatureService.setCharacteristic(Characteristic.CurrentTemperature, this.temperature);
-		callback(null, temperature);
-		}.bind(this));
+APCAccessory.prototype = {
+    getState: function(callback) {
+        ups.getTemperature(function(err, temperature) {
+            if (err) {
+                console.log(err.toString());
+                callback(error);
+                return;
+            }
+            this.log('The current temperature is:', temperature, 'C');
+            this.temperature = temperature;
+            temperatureService.setCharacteristic(Characteristic.CurrentTemperature, this.temperature);
+            callback(null, temperature);
+        }.bind(this));
 
-	},
-        identify: function (callback) {
-            this.log("Identify requested!");
-            callback(); // success
-        },
+    },
+    identify: function(callback) {
+        this.log("Identify requested!");
+        callback(); // success
+    },
 
-        getServices: function () {
-            var informationService = new Service.AccessoryInformation();
+    getServices: function() {
+        var informationService = new Service.AccessoryInformation();
 
-            informationService
-                .setCharacteristic(Characteristic.Manufacturer, "OpenWeatherMap")
-                .setCharacteristic(Characteristic.Model, "Location")
-                .setCharacteristic(Characteristic.SerialNumber, "");
+        informationService
+            .setCharacteristic(Characteristic.Manufacturer, "OpenWeatherMap")
+            .setCharacteristic(Characteristic.Model, "Location")
+            .setCharacteristic(Characteristic.SerialNumber, "");
 
-            temperatureService = new Service.TemperatureSensor(this.name);
-            temperatureService
-                .getCharacteristic(Characteristic.CurrentTemperature)
-                .on("get", this.getState.bind(this));
+        temperatureService = new Service.TemperatureSensor(this.name);
+        temperatureService
+            .getCharacteristic(Characteristic.CurrentTemperature)
+            .on("get", this.getState.bind(this));
 
-            temperatureService
-                .getCharacteristic(Characteristic.CurrentTemperature)
-                .setProps({minValue: -30});
+        temperatureService
+            .getCharacteristic(Characteristic.CurrentTemperature)
+            .setProps({
+                minValue: -30
+            });
 
-            temperatureService
-                .getCharacteristic(Characteristic.CurrentTemperature)
-                .setProps({maxValue: 120});
+        temperatureService
+            .getCharacteristic(Characteristic.CurrentTemperature)
+            .setProps({
+                maxValue: 120
+            });
 
-            return [informationService, temperatureService];
-        }
+        return [informationService, temperatureService];
+    }
 
 
-    };
-
+};
