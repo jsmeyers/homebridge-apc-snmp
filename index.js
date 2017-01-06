cd811be88f7648348b919c4f1edaafec84992d1a
jsmeyers <jsmeyers@users.noreply.github.com>
2017-01-06 18:18:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,72 +17,72 @@ function APCAccessory(log, config) {
     this.lastupdate = 0;
 }
 
-<<<<<<< HEAD
-APCAccessory.prototype =
-    {
-        getState: function (callback) {
-		ups.getTemperature(function(err, temperature) {
-  		if (err) {
-    		console.log(err.toString());
-callback(err);    		
-return;
-  		}	
-		this.log('The current temperature is:', temperature, 'C');
-		this.temperature = temperature;
-		temperatureService.setCharacteristic(Characteristic.CurrentTemperature, this.temperature);
-		
-		this.log('The current temperature is:', temperature, 'C');
-		callback(null, temperature);
-		}.bind(this));
-=======
+<<
+<< << < HEAD
 APCAccessory.prototype = {
-    getState: function(callback) {
-        ups.getTemperature(function(err, temperature) {
-            if (err) {
-                console.log(err.toString());
-                callback(error);
-                return;
-            }
-            this.log('The current temperature is:', temperature, 'C');
-            this.temperature = temperature;
-            temperatureService.setCharacteristic(Characteristic.CurrentTemperature, this.temperature);
-            callback(null, temperature);
-        }.bind(this));
->>>>>>> 020de74e2b8954bdd4846fbf1df6377e6791c72c
+        getState: function(callback) {
+                ups.getTemperature(function(err, temperature) {
+                    if (err) {
+                        console.log(err.toString());
+                        callback(err);
+                        return;
+                    }
+                    this.log('The current temperature is:', temperature, 'C');
+                    this.temperature = temperature;
+                    temperatureService.setCharacteristic(Characteristic.CurrentTemperature, this.temperature);
 
-    },
-    identify: function(callback) {
-        this.log("Identify requested!");
-        callback(); // success
-    },
-
-    getServices: function() {
-        var informationService = new Service.AccessoryInformation();
-
-        informationService
-            .setCharacteristic(Characteristic.Manufacturer, "OpenWeatherMap")
-            .setCharacteristic(Characteristic.Model, "Location")
-            .setCharacteristic(Characteristic.SerialNumber, "");
-
-        temperatureService = new Service.TemperatureSensor(this.name);
-        temperatureService
-            .getCharacteristic(Characteristic.CurrentTemperature)
-            .on("get", this.getState.bind(this));
-
-        temperatureService
-            .getCharacteristic(Characteristic.CurrentTemperature)
-            .setProps({
-                minValue: -30
-            });
-
-        temperatureService
-            .getCharacteristic(Characteristic.CurrentTemperature)
-            .setProps({
-                maxValue: 120
-            });
-
-        return [informationService, temperatureService];
-    }
+                    this.log('The current temperature is:', temperature, 'C');
+                    callback(null, temperature);
+                }.bind(this)); ===
+                === =
+                APCAccessory.prototype = {
+                    getState: function(callback) {
+                        ups.getTemperature(function(err, temperature) {
+                            if (err) {
+                                console.log(err.toString());
+                                callback(error);
+                                return;
+                            }
+                            this.log('The current temperature is:', temperature, 'C');
+                            this.temperature = temperature;
+                            temperatureService.setCharacteristic(Characteristic.CurrentTemperature, this.temperature);
+                            callback(null, temperature);
+                        }.bind(this));
 
 
-};
+                    },
+                    identify: function(callback) {
+                        this.log("Identify requested!");
+                        callback(); // success
+                    },
+
+                    getServices: function() {
+                        var informationService = new Service.AccessoryInformation();
+
+                        informationService
+                            .setCharacteristic(Characteristic.Manufacturer, "OpenWeatherMap")
+                            .setCharacteristic(Characteristic.Model, "Location")
+                            .setCharacteristic(Characteristic.SerialNumber, "");
+
+                        temperatureService = new Service.TemperatureSensor(this.name);
+                        temperatureService
+                            .getCharacteristic(Characteristic.CurrentTemperature)
+                            .on("get", this.getState.bind(this));
+
+                        temperatureService
+                            .getCharacteristic(Characteristic.CurrentTemperature)
+                            .setProps({
+                                minValue: -30
+                            });
+
+                        temperatureService
+                            .getCharacteristic(Characteristic.CurrentTemperature)
+                            .setProps({
+                                maxValue: 120
+                            });
+
+                        return [informationService, temperatureService];
+                    }
+
+
+                };
