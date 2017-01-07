523d0c75711987bd6fa1bb91627ede9669c38dde
jsmeyers <jsmeyers@users.noreply.github.com>
2017-01-07 11:48:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ APCAccessory.prototype = {
 	       callback(null, temperature);
              
    });
- console.log("callback");
+ console.log(callback);
    },
    identify: function(callback) {
  this.log("Identify requested!");
